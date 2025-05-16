8d67a49a628bb5aa9625f9d97364c2003eb1c443
Charlene Jiang <charlene.jiang06@gmail.com>
2025-05-17 01:32:57 +0200

    moved code from previous branch, after the restructuring of the file tree

--- Projet/include/env_joueur.h
+++ Projet/include/env_joueur.h
@@ -12,9 +12,23 @@
 
 class EnvJoueur {
 	std::string pseudo;
+	std::unordered_map<Position, Tuile> tuiles;//la map, pas beosin de désallouer à la fin, on stocke directement les tuiles dans unordered map par valeur
 	size_t nbJetonNature;
 	size_t scoreFinal;
 
 public:
-	
+	void placerTuile(const Position& coord, const Tuile& tuile) {
+		tuiles[coord] = tuile;
+	};
+
+	//check si ya uen tuile sur cette case
+	bool aTuile(const Position& coord) const {
+		return tuiles.find(coord) != tuiles.end(); //find trouve la tuile avec la clé coord (renvoie tuiles.end() sinon), end renvoie pointeur vers l'élément après le dernier de tuiles
+	};
+
+	//get pointeur de la tuile à cette coordonnée
+	const Tuile* getTuile(const Position& coord) const;
+
+	//est ce qu'on peut placer une tuile à cette coordonnée?
+	bool positionValide(const Position& coord) const;
 };
--- Projet/src/env_joueur.cpp
+++ Projet/src/env_joueur.cpp
@@ -4,5 +4,36 @@
 #include <unordered_map>
 #include <optional>
 #include <stdexcept>
+#include "position.h"
+#include "tuile.h"
 #include "env_joueur.h"
 
+
+//get pointeur de la tuile à cette coordonnée
+const Tuile* EnvJoueur::getTuile(const Position& coord) const {
+	auto it = tuiles.find(coord);//on trouve la paire <Position,Tuile>
+	if (it != tuiles.end()) {//si elle existe, alors on retourne pointeur vers la tuile
+		return &(it->second);
+	}
+	return nullptr;
+}
+
+
+//est ce qu'on peut placer une tuile à cette coordonnée?
+bool EnvJoueur::positionValide(const Position& coord) const {
+	//hyp: une tuile pour être placé, doit être au moins adjacente à une autre tuile
+	//est ce qu'il y a déjà une tuile dans cette position?
+	if (tuiles.find(coord) != tuiles.end()) {
+		//donc il y a déjà une tuile à cette endroit.
+		return false;
+	}
+	//est ce qu'il y a au moins une tuile adjacente à cette position? si oui donc valide
+	auto voisins = coord.getVecteurPositionsAdjacentes();
+	for (const auto& v : voisins) {
+		if (getTuile(v)) {
+			return true;
+		}
+	}
+	//aucune tuile adjacente (aucun voisin)
+	return false;
+};
